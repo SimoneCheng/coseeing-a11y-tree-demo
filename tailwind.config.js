5ab4ba3e673314f648c06c53cb882a11103a169a
SimoneCheng <bleach1827@gmail.com>
2025-11-25 18:53:42 +0800

    feat: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,19 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		colors: {
-			teal: {
-				100: "#718A92",
-				200: "#566F79",
-				300: "#3A5961",
-				PRIMARY: "#1F4147",
-				500: "#1A373E",
-				600: "#152D34",
-				700: "#10242A",
+		extend: {
+			colors: {
+				teal: {
+					100: "#718A92",
+					200: "#566F79",
+					300: "#3A5961",
+					PRIMARY: "#1F4147",
+					500: "#1A373E",
+					600: "#152D34",
+					700: "#10242A",
+				},
 			},
 		},
-		extend: {},
 	},
 	plugins: [],
 };
